f8f9681ead911e3228978d78fb4c1d9183d5d891
Albx68 <albertyumnam3D@gmail.com>
2022-11-19 20:20:07 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { useId, useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { atom, useAtom } from "jotai";
+import { useAtom } from "jotai";
 import {
   counterAtom,
   decrementCounterAtom,
   incrementCounterAtom,
 } from "../utils/atoms";
 function App() {
-  const [count,] = useAtom(counterAtom);
-  const [, decrement] = useAtom(decrementCounterAtom); 
+  const [count] = useAtom(counterAtom);
+  const [, decrement] = useAtom(decrementCounterAtom);
   //decrementCounterAtom is a read and write atom, imported only the write function here
   const [, increment] = useAtom(incrementCounterAtom);
-   //incrementCounterAtom is a read and write atom, imported only the write function here
-  const id = useId()
+  //incrementCounterAtom is a read and write atom, imported only the write function here
+
   return (
     <div className="App">
-      <div className="counter"> 
-        <div className="count" key={Math.random()}>{count}</div> 
-        {/*the random key is to trigger css animations on count change*/}
+      <div className="counter">
+        <div className="count" key={Math.random()}>
+          {/*the random key is to trigger css animations on count change*/}
+          {count}
+        </div>
         <div className="btns">
           <button onClick={decrement}>-</button>
           <button onClick={increment}>+</button>
